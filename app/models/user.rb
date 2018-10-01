f2a76637790130b63cb833403d3030bbf1cbcfcc
Flatiron School <andrewsprague@outlook.com>
2018-10-01 16:45:37 -0500

    add password gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+    has_secure_password
+    
     has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
 
     has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -9,6 +12,5 @@ class User < ApplicationRecord
     has_many :beards
     has_many :ratings, through: :beards
 
-    has_secure_password
 
 end
