88ece4679d93ac255031c0f25e93605700467293
Flatiron School <andrewsprague@outlook.com>
2018-10-02 11:59:11 -0500

    added header functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
     def create
+      
       @user = User.find_by(user_name: params[:user_name])
-      return head(:forbidden) unless @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to @user
+      if @user == nil || !@user.authenticate(params[:password])
+        flash[:errors] = "Incorrect username/password"
+        redirect_to kenny_login_path
+      else
+        session[:user_id] = @user.id
+        redirect_to @user  
+      end
+      # return head(:forbidden) unless @user.authenticate(params[:password])
+      # byebug
+      # session[:user_id] = @user.id
+      # redirect_to @user
     end
 
     def destroy
